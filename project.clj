646117a395d883a3788bc3a6f57f65ecdef6e6f2
Brad Lucas <brad@beaconhill.com>
2016-10-23 09:34:55 -0400

    version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject depends "0.1.0-SNAPSHOT"
+(defproject depends "1.0.0"
   :description "Walks a directory structure, parses project.clj files for dependencies and produces a report"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
